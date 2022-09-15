42e62d252517537b02f481f42836f3ce7c014a0a
EndryoMachado <89991280+EndryoMachado@users.noreply.github.com>
2022-09-14 21:49:46 -0300

    Update bdprojeto.sql

--- banco/bdprojeto.sql
+++ banco/bdprojeto.sql
@@ -12,11 +12,10 @@ id_usuario int not null primary key auto_increment,
 nome_usuario varchar(45) not null,
 email varchar(45) not null,
 senha varchar(45) not null,
+status char(2) not null,
 telefone int not null,
 foreign key(telefone) references telefone(id_telefone)
 );
 /*
 Select * from usuario inner join telefone on usuario.telefone = id_telefone;
-insert into telefone(numero) values ('92069660');
-insert into usuario(nome_usuario, email, senha, telefone) values ( 'Endryo', 'endryo@machado.com', '1234', last_insert_id());
 */
